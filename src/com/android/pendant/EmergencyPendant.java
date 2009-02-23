c183fcc3c489b78f5c0f7bc09f18c91985e2f808
unknown <Dan@.(none)>
2009-02-23 17:46:38 -0600

    some more stuff editted

--- src/com/android/pendant/EmergencyPendant.java
+++ src/com/android/pendant/EmergencyPendant.java
@@ -2,12 +2,14 @@ package com.android.pendant;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+import android.content.Intent;
 public class EmergencyPendant extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent svc = new Intent(this, InputWrapper.class);
+        startService(svc);
         setContentView(R.layout.main);
     }
 }
